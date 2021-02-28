d1ac187574c6a32794e8d902a1ef34baf705de08
Rob Ruff <rruff@users.noreply.github.com>
2021-02-27 23:18:10 -0500

    Add files via upload

---
+++ discogs.py
@@ -0,0 +1,18 @@
+#!/usr/local/bin/python3
+
+import json
+import requests
+
+token = "AxflzoOJHIkEtHOKiBcNqUHYTtdgogApyPbCbSQv"
+base_url = "https://api.discogs.com/"
+user = "robruff"
+user_agent = "FooBarApp/3.0"
+
+def list_folders(user):
+    url = base_url + "users/" + user + "/collection/folders?token=" + token
+    return requests.get(url)
+
+if __name__ == "__main__":
+    resp = list_folders(user)
+    json_out = json.loads(resp.text)
+    print(json.dumps(json_out, indent=4))
