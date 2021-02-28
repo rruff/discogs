87ad561804326d3f13770214e13c2f9c2271f335
Rob Ruff <robruff@gmail.com>
2021-02-28 17:24:17 -0500

    Added config file and code to parse it.

--- discogs.py
+++ discogs.py
@@ -3,16 +3,32 @@
 import json
 import requests
 
-token = ""
-base_url = "https://api.discogs.com/"
-user = "robruff"
-user_agent = "FooBarApp/3.0"
+_config = None
 
-def list_folders(user):
-    url = base_url + "users/" + user + "/collection/folders?token=" + token
+def load_config():
+    fp = open('config.json', 'r')
+    return json.load(fp)
+
+def list_folders(user, usertoken=None):
+    url = _config.baseurl + _config.paths['users'] + "/" + user + "/" + _config.paths['folders']
+    if usertoken:
+        url += "?token=" + usertoken
+    
     return requests.get(url)
 
+class Config:
+    def __init__(self, filename):
+        _conf = self._loadconfig(filename)
+        self.baseurl = _conf['baseurl']
+        self.user = _conf['user']
+        self.paths = _conf['paths']
+    
+    def _loadconfig(self, filename):
+        fp = open(filename, 'r')
+        return json.load(fp)
+        
 if __name__ == "__main__":
-    resp = list_folders(user)
+    _config = Config('config.json')
+    resp = list_folders(_config.user)
     json_out = json.loads(resp.text)
     print(json.dumps(json_out, indent=4))
